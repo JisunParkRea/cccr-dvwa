7e1c99c6618324d188ce734cb103426cb429ecec
JisunParkRea <fpdldk912@gmail.com>
2020-10-16 18:23:06 +0900

    make resources/base directories

---
+++ src/test/java/com/appsecco/dvja/controllers/UserAuthenticationServiceTest.java
@@ -0,0 +1,33 @@
+package com.appsecco.dvja.controllers;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+import com.appsecco.dvja.models.User;
+import com.appsecco.dvja.services.UserAuthenticationService;
+import org.apache.commons.lang.StringUtils;
+
+
+public class LoginTest {
+	UserAuthenticationService userAuthenticationService;
+	
+	private static final String TEST_LOGIN = "noLogin";
+	private static final String TEST_PASSWD = "noPasswd";
+
+
+	UserAuthenticationService u;
+
+	@Before
+    public void setUp() {
+    	u = new UserAuthenticationService();
+    }
+
+	@Test
+	public void testAuthenticate() {
+		String login = "noLogin";
+		String password = "noPasswd";
+
+		User user = u.authenticate(login, password);
+		assertNull(user);
+	}
+}
