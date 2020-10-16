76a4d7b1a198869b0b1d87652f13154dc46812db
JisunParkRea <fpdldk912@gmail.com>
2020-10-16 15:04:01 +0900

    fail test

--- src/test/java/UserAuthenticationServiceTest.java
+++ src/test/java/UserAuthenticationServiceTest.java
@@ -7,8 +7,11 @@ import static org.junit.Assert.*;
 public class UserAuthenticationServiceTest {
 	@Test
 	public void testAuthenticate() {
-		UserAuthenticationService u = new UserAuthenticationService();
-		User user = u.authenticate('noLogin', 'noPassword');
-		assertEquals(null, user, 0);
+		// UserAuthenticationService u = new UserAuthenticationService();
+
+		// User user = u.authenticate("noLogin", "noPassword");
+
+		// assertEquals(null, user, 0);
+		fail('failed');
 	}
 }
