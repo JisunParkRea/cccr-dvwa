abfd3e663fae1b42e3dcfe8b4cdd33c84896d7ff
Jisun Park <fpdldk912@gmail.com>
2020-10-24 10:14:55 +0900

    Update schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -40,7 +40,7 @@ CREATE TABLE `products` (
 -- Table structure for table `users`
 --
 
-DROP TABLE IF EXISTS `users`;
+/*DROP TABLE IF EXISTS `users`;*/
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `users` (
