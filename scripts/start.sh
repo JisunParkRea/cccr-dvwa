c92d2738f8c40a54743c1220610de6a0e7619cad
Jisun Park <fpdldk912@gmail.com>
2020-11-13 15:05:59 +0900

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -6,4 +6,4 @@ sleep 10
 mysql -h mysql -u $MYSQL_USER -p$MYSQL_PASSWORD -e "CREATE DATABASE dvja CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci"
 mysql -h mysql -u $MYSQL_USER -p$MYSQL_PASSWORD dvja < ./db/schema.sql
 
-mvn jetty:run
+mvn jetty:run -Dcheckstyle.skip -Dspotbugs.skip -Dpmd.skip
